3582ad62d865409ecaadecb4c120516f8d218457
Hilko <91159261+HilkoCFisi@users.noreply.github.com>
2021-10-18 08:59:38 +0200

    Update Software-install.ps1

--- Software-install.ps1
+++ Software-install.ps1
@@ -1,1 +1,234 @@
+#######################################################################
+# Tutorial: https://www.youtube.com/watch?v=1A0GrK51cwY&list=PLaYEWfRap9wh5UZHBzMeYuEKGpK5-_-PJ&index=91
+# Choco: https://chocolatey.org/install
+# Packages: https://community.chocolatey.org/packages
+#######################################################################
 
+# Installiere Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+###################################
+
+# Software installieren
+
+#choco install opera-gx
+#a
+#choco upgrade opera-gx
+
+#choco install python3
+#a
+#choco upgrade python3
+
+choco install winrar
+#a
+choco upgrade winrar
+
+choco install adobereader
+#a
+choco upgrade adobereader
+
+#choco install vscode
+#a
+#choco upgrade vscode
+
+#choco install vmware-workstation-player
+#a
+#choco upgrade vmware-workstation-player
+
+#choco install malwarebytes
+#a
+#choco upgrade malwarebytes
+
+#choco install vlc
+#a
+#choco upgrade vlc
+
+#choco install jre8
+#a
+#choco upgrade jre8
+
+#choco install putty
+#a
+#choco upgrade putty
+
+#choco install ccleaner
+#a
+#choco upgrade ccleaner
+
+#choco install teamviewer
+#a
+#choco upgrade teamviewer
+
+#choco install gimp
+#a
+#choco upgrade gimp
+
+#choco install atom
+#a
+#choco upgrade atom
+
+#choco install wireshark
+#a
+#choco upgrade wireshark
+
+#choco install octopusdeploy.tentacle
+#a
+#choco upgrade octopusdeploy.tentacle
+
+#choco install audacity
+#a
+#choco upgrade audacity
+
+choco install openoffice
+#a
+choco upgrade openoffice
+
+#choco install rufus
+#a
+#choco upgrade rufus
+
+#choco install eclipse
+#a
+#choco upgrade eclipse
+
+#choco uninstall onedrive
+#a
+
+#choco install blender
+#a
+#choco upgrade blender
+
+#choco install github-desktop
+#a
+#choco upgrade github-desktop
+
+#choco install sharex
+#a
+#choco upgrade sharex
+
+#choco install obs-studio
+#a
+#choco upgrade obs-studio
+
+#choco install epicgameslauncher
+#a
+#choco upgrade epicgameslauncher
+
+#choco install goggalaxy
+#a
+#choco upgrade goggalaxy
+
+#choco install steam-client
+#a
+#choco upgrade steam-client
+
+#choco install battle.net
+#a
+#choco upgrade battle.net
+
+#choco install teamspeak
+#a
+#choco upgrade teamspeak
+
+#choco install twitch
+#a
+#choco upgrade twitch
+
+choco install msiafterburner
+#a
+choco upgrade msiafterburner
+
+#choco install xmind
+#a
+#choco upgrade xmind
+
+#choco install windowsrepair
+#a
+#choco upgrade windowsrepair
+
+#choco install streamlink
+#a
+#choco upgrade streamlink
+
+#choco install patch-my-pc
+#a
+#choco upgrade patch-my-pc
+
+#choco install officeproplus2013
+#a
+
+#choco install glasswire
+#a
+#choco upgrade glasswire
+
+#choco install speedfan
+#a
+#choco upgrade speedfan
+
+#choco install win32diskimager
+#a
+#choco upgrade win32diskimager
+
+#choco install minecraft
+#a
+#choco upgrade minecraft
+
+#choco install atubecatcher
+#a
+#choco upgrade atubecatcher
+
+#choco install mediathekview
+#a
+#choco upgrade mediathekview
+
+#choco install furmark
+#a
+#choco upgrade furmark
+
+#choco install freevideoeditor
+#a
+#choco upgrade freevideoeditor
+
+#choco install onlyoffice
+#a
+#choco upgrade onlyoffice
+
+#choco install streamdeck
+#a
+#choco upgrade streamdeck
+
+#choco install ultraiso
+#a
+#choco upgrade ultraiso
+
+#choco install xmedia-recode
+#a
+#choco upgrade xmedia-recode
+
+#choco install produkey
+#a
+
+#choco install lastactivityview
+#a
+#choco upgrade lastactivityview
+
+#choco install hitmanpro
+#a
+#choco upgrade hitmanpro
+
+#choco install synfig
+#a
+#choco upgrade synfig
+
+#choco install lossless-cut
+#a
+#choco upgrade lossless-cut
+
+#choco install tv-browser
+#a
+
+#choco install systemninja
+#a
+
+#choco install xml-notepad
+#a
